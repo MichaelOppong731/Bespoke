d056aeea520b573ea74f1e993cedd22d80b78200
MichaelOppong731 <michaeloppong731@gmail.com>
2024-04-03 04:48:51 +0000

    more pages added

---
+++ src/main/java/com/bespoke/Bespoke/service/AuthenticationSuccessHandler.java
@@ -0,0 +1,33 @@
+package com.bespoke.Bespoke.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+@Service
+public class AuthenticationSuccessHandler implements org.springframework.security.web.authentication.AuthenticationSuccessHandler {
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException, ServletException {
+        // TODO Auto-generated method stub
+
+        var authourities = authentication.getAuthorities();
+        var roles = authourities.stream().map(r -> r.getAuthority()).findFirst();
+
+        if (roles.orElse("").equals("ADMIN")) {
+            response.sendRedirect("/admin/home");
+        } else if (roles.orElse("").equals("USER")) {
+            response.sendRedirect("/user/home");
+        } else {
+            response.sendRedirect("/error");
+        }
+
+
+    }
+}
