f53d4683460949039fca0c57ed5d4ea90dc67af7
MichaelOppong731 <michaeloppong731@gmail.com>
2024-05-08 13:36:08 +0000

    Updated Config Files

--- src/main/java/com/bespoke/Bespoke/security/config/WebSecurityConfig.java
+++ src/main/java/com/bespoke/Bespoke/security/config/WebSecurityConfig.java
@@ -35,6 +35,7 @@ public class WebSecurityConfig {
                 .csrf(c -> c.disable())
                 .authorizeHttpRequests(request -> {request.requestMatchers("/login", "/register"
                 ,"/password-request", "/reset-password").permitAll();
+                request.requestMatchers("/css/**", "/js/**").permitAll();//Allow static files
                 request.requestMatchers("/Admin/**","/upload").hasAuthority("ROLE_ADMIN");
                 request.requestMatchers("/User/**").hasAuthority("ROLE_USER");
                 request.requestMatchers("/video/**").hasAnyAuthority("ROLE_USER","ROLE_ADMIN");
